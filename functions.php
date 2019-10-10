5944a334935e466658825b22c1c03bb6f05d6b75
Emre Can ÖZTAŞ <me@emrecanoztas.com>
2019-10-10 18:07:24 +0300

    Add files via upload

---
+++ functions.php
@@ -0,0 +1,26 @@
+<?php
+
+# ACTIONS
+
+# FILTERS
+
+# FUNCTIONS
+function add_theme_style_files()
+{
+	# code..
+}
+
+function add_theme_script_files()
+{
+	# code...
+}
+
+function create_theme_sidebars()
+{
+	# code...
+}
+
+function create_theme_logo()
+{
+	# code...
+}
